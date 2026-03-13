dda6040988e9fe697fdb0bee9295e2f03230ba68
Autumn-T-T <thomas.autumnalicia@gmail.com>
2026-03-12 21:22:35 -0400

    Start server and listen on port 3000

--- server.js
+++ server.js
@@ -6,6 +6,11 @@ const port = 3000;
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public/index.html'));
 });
+
 app.get('/contact', (req, res) => {
   res.sendFile(path.join(__dirname, 'public/contact.html'));
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
 });
